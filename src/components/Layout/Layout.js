5dd319d437b987927f8149e6150c2f61c07df042
Justin Gallardo <jgallardorandelle@gmail.com>
2018-02-26 22:28:24 +0800

    finish side drawer

--- src/components/Layout/Layout.js
+++ src/components/Layout/Layout.js
@@ -1,17 +1,32 @@
-import React from 'react';
+import React, { Component } from 'react';
 
 import Aux from '../../hoc/Auxilliary';
 import classes from './Layout.css';
 import Toolbar from '../Navigation/Toolbar/Toolbar';
 import SideDrawer from '../Navigation/SideDrawer/SideDrawer';
-const layout = ( props ) => (
-    <Aux>
-        <Toolbar/>
-        <SideDrawer />
-        <main className={classes.Content}>
-            {props.children}
-        </main>
-    </Aux>
-);
 
-export default layout;
+class Layout extends Component {
+    state = {
+        showSideDrawer: false
+    }
+
+    SideDrawerToggle = () => {
+        this.setState((prevState) => {
+            return { showSideDrawer: !prevState.showSideDrawer }
+        });
+    }
+
+    render() {
+        return (
+            <Aux>
+                <Toolbar clicked={this.SideDrawerToggle} />
+                <SideDrawer open={this.state.showSideDrawer}
+                    closed={this.SideDrawerToggle} />
+                <main className={classes.Content}>
+                    {this.props.children}
+                </main>
+            </Aux>)
+    }
+};
+
+export default Layout;
--- src/components/Navigation/Toolbar/Toolbar.js
+++ src/components/Navigation/Toolbar/Toolbar.js
@@ -2,14 +2,15 @@ import React from 'react';
 import classes from './Toolbar.css';
 import Logo from '../../Logo/Logo';
 import NavigationItems from '../NavigationItems/NavigationItems';
+import DrawerToggler from '../SideDrawer/DrawerToggler/DrawerToggler';
 const toolbar = (props) => (
     <header className={classes.Toolbar}>
-        <div>Menu</div>
-        <Logo />
-        <nav>
+        <DrawerToggler clicked={props.clicked} />
+        <div className={classes.Logo}>
+            <Logo />
+        </div>
+        <nav className={classes.DesktopOnly}>
             <NavigationItems />
-
-
         </nav>
     </header>
 );
