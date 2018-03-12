45a69e5e72fc92b72eb45060b095f78e8a4309ea
Justin Gallardo <jrgallardo@fastgroup.biz>
2018-03-12 18:15:34 +0800

    added two way binding

--- src/containers/Checkout/ContactData/ContactData.js
+++ src/containers/Checkout/ContactData/ContactData.js
@@ -1,17 +1,61 @@
 import React, { Component } from 'react';
 import Button from '../../../components/UI/Button/Button';
-import Spinner from '../../../components/UI/Spinner/Spinner';  
+import Spinner from '../../../components/UI/Spinner/Spinner';
 import axios from '../../../axios-orders';
-
+import Input from '../../../components/UI/Input/Input';
 import classes from './ContactData.css'
 
 class ContactData extends Component {
     state = {
-        name: '',
-        email: '',
-        address: {
-            street: '',
-            postalCode: ''
+        orderForm: {
+            name: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Your name',
+                },
+                value: 'Justin Gallardo'
+            },
+
+            street: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Street',
+                },
+                value: ''
+            },
+            zipCode: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'ZIP CODE',
+                },
+                value: ''
+            },
+            country: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'text',
+                    placeholder: 'Country',
+                },
+                value: ''
+            },
+            email: {
+                elementType: 'input',
+                elementConfig: {
+                    type: 'email',
+                    placeholder: 'Email',
+                },
+                value: ''
+            },
+            deliveryMethod: {
+                elementType: 'select',
+                elementConfig: {
+                    options: [{ value: 'fastest', text: 'Fastest' }, { value: 'cheapest', text: 'Cheapest' }]
+                },
+                value: ''
+            },
         },
         loading: false
 
@@ -22,36 +66,46 @@ class ContactData extends Component {
         this.setState({ loading: true });
         const orderObj = {
             ingredients: this.props.ingredients,
-            price: this.props.price,
-            customer: {
-                name: 'Justin Gallardo',
-                address: {
-                    street: 'Margarita',
-                    zipCode: '1772',
-                    country: 'PH'
-                },
-                email: 'test@test.com'
-            },
-            deliveryMethod: 'fastest'
+            price: this.props.price
         }
         axios.post('/orders.json', orderObj)
-            .then(res => { 
+            .then(res => {
                 this.props.history.push("/");
             },
             err => console.log(err))
             .finally(res => this.setState({ loading: true }));
     }
 
-    render() {
+    inputChangedHandler = (event, identifier) => {
+        const updatedOrderForm = {
+            ...this.state.orderForm
+        }
+        const updatedFormEl = { ...updatedOrderForm[identifier] };
+        updatedFormEl.value = event.target.value;
+        updatedOrderForm[identifier] = updatedFormEl;
+        this.setState({orderForm  : updatedOrderForm});
+    }
 
+    render() {
+        const formElements = [];
+        for (let key in this.state.orderForm) {
+            formElements.push({
+                id: key,
+                config: this.state.orderForm[key]
+            })
+        }
         let form = (<form>
-            <input type="text" name="name" placeholder="your name" />
-            <input type="email" name="email" placeholder="your email" />
-            <input type="text" name="street" placeholder="your street" />
-            <input type="text" name="postal" placeholder="your postal" />
+            {formElements.map(formElement => (
+                <Input
+                    key={formElement.id}
+                    elementType={formElement.config.elementType}
+                    elementConfig={formElement.config.elementConfig}
+                    value={formElement.config.value}
+                    changed={(event) => this.inputChangedHandler(event, formElement.id)} />
+            ))}
             <Button btnType="Success" clicked={this.orderHandler}> Order </Button>
         </form>);
-        if(this.state.loading){
+        if (this.state.loading) {
             form = <Spinner />
         }
 
